ed99bf0bb87fde1020c43598fb56cd00b887c46f
Gabriel Feceoru <gabi.feceoru@gmail.com>
2022-01-07 12:06:24 +0100

    First working version

--- model.py
+++ model.py
@@ -23,32 +23,39 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        super().__init__()
+        super(DecoderRNN, self).__init__()
 
         self.embed = nn.Embedding(vocab_size, embed_size)
-        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
         self.linear = nn.Linear(hidden_size, vocab_size)
-    
+
+        self.hidden_size = hidden_size
+
     def forward(self, features, captions):
         # remove the <end> from captions
         embeddings = self.embed(captions[:, :-1])
 
-        # Concatenate the feature and captions
+        # Concatenate the features and captions
         inputs = torch.cat((features.unsqueeze(1), embeddings), dim=1)
-
-        hidden, states = self.lstm(inputs, states=None)
-
+        hidden, states = self.lstm(inputs)
         outputs = self.linear(hidden)
+
         return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
+
+        states = (torch.randn(1, 1, self.hidden_size).to(inputs.device),
+                  torch.randn(1, 1, self.hidden_size).to(inputs.device))
+
         sentence = []
         for i in range(max_len):
             hidden, states = self.lstm(inputs, states)
             scores = self.linear(hidden)
-            _, predicted = scores.max(dim=2)
+            predicted = torch.argmax(scores, dim=2)
             sentence.append(predicted.item())
+            if predicted.item() == 1:
+                break
             inputs = self.embed(predicted)
 
         return sentence

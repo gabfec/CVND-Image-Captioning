f4e9516f9dff4aacecc36a3eb87e1261fbad52e2
Gabriel Feceoru <gabi.feceoru@gmail.com>
2022-01-07 11:59:43 +0100

    Add model implementation

--- model.py
+++ model.py
@@ -23,11 +23,32 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        # remove the <end> from captions
+        embeddings = self.embed(captions[:, :-1])
+
+        # Concatenate the feature and captions
+        inputs = torch.cat((features.unsqueeze(1), embeddings), dim=1)
+
+        hidden, states = self.lstm(inputs, states=None)
+
+        outputs = self.linear(hidden)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        sentence = []
+        for i in range(max_len):
+            hidden, states = self.lstm(inputs, states)
+            scores = self.linear(hidden)
+            _, predicted = scores.max(dim=2)
+            sentence.append(predicted.item())
+            inputs = self.embed(predicted)
+
+        return sentence
